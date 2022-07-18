3ff60f22b147388077788d3634e70547b9cc7ee6
getjetdrive <109487870+getjetdrive@users.noreply.github.com>
2022-07-19 03:56:51 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 if ("ServiceWorker" in navigator){
   navigator.serviceWorker.register("/serviceWorker.js");
 }
-
+let installPromptEvent;
+		window.addEventListener('beforeinstallprompt', (event) => {
+		  installPromptEvent = event;
+		});
